19ce9d580e6c06787e03ad0ef83cf565fe297202
Sasaki-Motoaki <voyegesansretour1@gmail.com>
2021-12-23 23:29:47 +0900

    公式例をコピペして確認 https://github.com/cdk8s-team/cdk8s/blob/master/examples/go/hello/main.go

--- cdk8s/main.go
+++ cdk8s/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"msasaki666/cdk8s-prac/imports/k8s"
+
 	"github.com/aws/constructs-go/constructs/v3"
 	"github.com/aws/jsii-runtime-go"
 	"github.com/cdk8s-team/cdk8s-core-go/cdk8s"
 )
 
+// 全リソース共通のlabel、デフォルトnamespaceを定義するための構造体
 type MyChartProps struct {
 	cdk8s.ChartProps
 }
@@ -17,7 +20,39 @@ func NewMyChart(scope constructs.Construct, id string, props *MyChartProps) cdk8
 	}
 	chart := cdk8s.NewChart(scope, jsii.String(id), &cprops)
 
-	// define resources here
+	label := map[string]*string{"app": jsii.String("hello-k8s")}
+
+	k8s.NewKubeService(chart, jsii.String("service"), &k8s.KubeServiceProps{
+		Spec: &k8s.ServiceSpec{
+			Type: jsii.String("LoadBalancer"),
+			Ports: &[]*k8s.ServicePort{{
+				Port:       jsii.Number(80),
+				TargetPort: k8s.IntOrString_FromNumber(jsii.Number(8000)),
+			}},
+			Selector: &label,
+		},
+	})
+
+	k8s.NewKubeDeployment(chart, jsii.String("deployment"), &k8s.KubeDeploymentProps{
+		Spec: &k8s.DeploymentSpec{
+			Replicas: jsii.Number(2),
+			Selector: &k8s.LabelSelector{
+				MatchLabels: &label,
+			},
+			Template: &k8s.PodTemplateSpec{
+				Metadata: &k8s.ObjectMeta{
+					Labels: &label,
+				},
+				Spec: &k8s.PodSpec{
+					Containers: &[]*k8s.Container{{
+						Name:  jsii.String("hello-kubernetes"),
+						Image: jsii.String("paulbouwer/hello-kubernetes:1.7"),
+						Ports: &[]*k8s.ContainerPort{{ContainerPort: jsii.Number(8080)}},
+					}},
+				},
+			},
+		},
+	})
 
 	return chart
 }
